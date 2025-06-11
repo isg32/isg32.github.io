cf8a05138c8e57ff56551e315ba9677484eb6f91
isg32 <sapangajjar101105@gmail.com>
2025-06-11 17:40:05 +0530

    update with the blogpost dynamic
    Signed-off-by: isg32 <sapangajjar101105@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import Homepage from './pages/Home'
+import Posts from './pages/Posts'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <Navbar />
-      <div className="">
-      <Homepage/></div>
-      <p className="footer">
+    <Navbar />
+    <div className="">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage/>} />
+          <Route path="/posts" element={<Posts/>} />
+        </Routes>
+      </Router>
+    </div>
+    <p className="footer">
       © Copyright 2025 Sapan Gajjar. All rights reserved.  
-      </p>
+    </p>
     </>
   )
 }
